cfd1506ba360f6070058bf335fd2816328a691fe
Kasea <1997xeela@gmail.com>
2018-08-19 19:16:38 +0200

    fixed glitch

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ class NoMoreDesyncMasterFinalEditionMasterKaseasGodPlanToStopWipingToStupidShitO
         function ayy_lmfao_remove_desyncs_kthx_fam(e) {
             if(entity.mobs[e.gameId.toString()] && (mobs[e.gameId.toString()] || 0) <= Date.now()) dispatch.send('S_INSTANT_MOVE', 3, e);
         }
-        dispatch.hook('S_ACTION_STAGE', ver['s'][dispatch.base.majorPatchVersion], ayy_lmfao_remove_desyncs_kthx_fam);
+        //dispatch.hook('S_ACTION_STAGE', ver['s'][dispatch.base.majorPatchVersion], ayy_lmfao_remove_desyncs_kthx_fam);
         dispatch.hook('S_ACTION_END', ver['e'][dispatch.base.majorPatchVersion], ayy_lmfao_remove_desyncs_kthx_fam);
 
         dispatch.hook('S_CREATURE_ROTATE', 2, e=> {
-            if(entity.mobs[e.gameId.toString()]) mobs[e.gameId.toString()] = Date.now() + e.time;
+            //if(entity.mobs[e.gameId.toString()]) mobs[e.gameId.toString()] = Date.now() + e.time;
         });
     }
 }
