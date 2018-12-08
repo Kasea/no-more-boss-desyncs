9bca31e7cc3a721c49de21dd6ea1e713f57ab82f
Kasea <1997xeela@gmail.com>
2018-12-08 17:35:37 +0100

    update (errors incoming poggers)

--- index.js
+++ index.js
@@ -1,33 +1,12 @@
 class NoMoreDesyncMasterFinalEditionMasterKaseasGodPlanToStopWipingToStupidShitOMEGALUL{
     constructor(dispatch) {
-        const { entity } = require('library')(dispatch);
-        const ver = {
-            "s": {
-                "72": 6,
-                "73": 6,
-                "74": 7,
-                "75": 8,
-                "76": 8,
-            },
-            "e": {
-                "72": 4,
-                "73": 4,
-                "74": 5,
-                "75": 5,
-                "76": 5
-            }
-        };
+        const { entity } = dispatch.require.library;
         let mobs = {};
 
         function ayy_lmfao_remove_desyncs_kthx_fam(e) {
             if(entity.mobs[e.gameId.toString()] && (mobs[e.gameId.toString()] || 0) <= Date.now()) dispatch.send('S_INSTANT_MOVE', 3, e);
         }
-        //dispatch.hook('S_ACTION_STAGE', ver['s'][dispatch.base.majorPatchVersion], ayy_lmfao_remove_desyncs_kthx_fam);
-        dispatch.hook('S_ACTION_END', ver['e'][dispatch.base.majorPatchVersion], ayy_lmfao_remove_desyncs_kthx_fam);
-
-        dispatch.hook('S_CREATURE_ROTATE', 2, e=> {
-            //if(entity.mobs[e.gameId.toString()]) mobs[e.gameId.toString()] = Date.now() + e.time;
-        });
+        dispatch.hook('S_ACTION_END', 5, ayy_lmfao_remove_desyncs_kthx_fam);
     }
 }
 
